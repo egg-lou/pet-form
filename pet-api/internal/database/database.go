7aa1e278ed076abbb72756884a674f948b8b0cb1
egglou <miguel.rafaellouie2003@gmail.com>
2024-06-16 17:10:19 +0800

    done table creation queries

--- pet-api/internal/database/database.go
+++ pet-api/internal/database/database.go
@@ -3,9 +3,11 @@ package database
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"fmt"
 	"log"
 	"os"
+	"pet-api/internal/queries"
 	"strconv"
 	"time"
 
@@ -38,18 +40,24 @@ var (
 )
 
 func New() Service {
-	// Reuse Connection
+	if dbInstance == nil {
+		Init()
+	}
+	return dbInstance
+}
+
+func Init() Service {
 	if dbInstance != nil {
 		return dbInstance
 	}
 
-	// Opening a driver typically will not attempt to connect to the database.
-	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", username, password, host, port, dbname))
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/", username, password, host, port))
 	if err != nil {
 		// This will not be a connection error, but a DSN parse error or
 		// another initialization error.
 		log.Fatal(err)
 	}
+
 	db.SetConnMaxLifetime(0)
 	db.SetMaxIdleConns(50)
 	db.SetMaxOpenConns(50)
@@ -57,6 +65,31 @@ func New() Service {
 	dbInstance = &service{
 		db: db,
 	}
+
+	err = dbInstance.createDatabaseIfNotExists()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Close the initial connection
+	err = db.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Reconnect, this time to the specific database
+	db, err = sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", username, password, host, port, dbname))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dbInstance.db = db
+
+	err = dbInstance.createTables()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	return dbInstance
 }
 
@@ -117,4 +150,58 @@ func (s *service) Health() map[string]string {
 func (s *service) Close() error {
 	log.Printf("Disconnected from database: %s", dbname)
 	return s.db.Close()
+}
+
+func (s *service) createDatabaseIfNotExists() error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	res, err := s.db.ExecContext(ctx, "CREATE DATABASE IF NOT EXISTS "+dbname)
+	if err != nil {
+		return err
+	}
+
+	rowsAffected, err := res.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rowsAffected == 0 {
+		return errors.New("failed to create database")
+	}
+
+	_, err = s.db.ExecContext(ctx, "USE"+dbname)
+
+	return nil
+}
+
+func (s *service) createTables() error {
+	//tables := []string{
+	//	queries.CreateOwnerTable,
+	//	queries.CreatePetTable,
+	//	queries.CreateVeterinarianTable,
+	//	queries.CreateServiceTable,
+	//	queries.CreatePreventiveCareTable,
+	//	queries.CreateGroomingTable,
+	//	queries.CreateSurgeryTable,
+	//}
+	tables := []string{
+		queries.CreateOwnerTable,
+		queries.CreatePetTable,
+		queries.CreateVeterinarianTable,
+		queries.CreateServiceTable,
+		queries.CreateServiceInstanceTable,
+		queries.CreatePreventiveCareTable,
+		queries.CreateGroomingTable,
+		queries.CreateSurgeryTable,
+	}
+
+	for _, table := range tables {
+		_, err := s.db.Exec(table)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
---
+++ pet-api/internal/queries/table_creation.go
@@ -0,0 +1,157 @@
+//package queries
+
+//const CreateOwnerTable = `CREATE TABLE IF NOT EXISTS owner (
+//    owner_id INT AUTO_INCREMENT PRIMARY KEY,
+//    owner_name VARCHAR(255) NOT NULL,
+//    owner_address VARCHAR(255) NOT NULL,
+//    owner_landline_number VARCHAR(255),
+//    owner_mobile_number VARCHAR(255) NOT NULL,
+//    owner_email_address VARCHAR(255) NOT NULL
+//)`
+//
+//const CreatePetTable = `CREATE TABLE IF NOT EXISTS pet(
+//    pet_id INT AUTO_INCREMENT PRIMARY KEY,
+//    owner_id INT NOT NULL,
+//    pet_name VARCHAR(255) NOT NULL,
+//    pet_birth_date DATE NOT NULL,
+//    pet_breed VARCHAR(255) NOT NULL,
+//    pet_color VARCHAR(255) NOT NULL,
+//    pet_type VARCHAR(255) NOT NULL,
+//
+//    FOREIGN KEY (owner_id) REFERENCES owner(owner_id)
+//)`
+//
+//const CreateVeterinarianTable = `CREATE TABLE IF NOT EXISTS veterinarian(
+//    vet_id INT AUTO_INCREMENT PRIMARY KEY,
+//    vet_name VARCHAR(255) NOT NULL,
+//    vet_email_address VARCHAR(50) NOT NULL,
+//    vet_license_number VARCHAR(255) NOT NULL
+//)`
+//
+//const CreateServiceTable = `CREATE TABLE IF NOT EXISTS service(
+//    service_id INT AUTO_INCREMENT PRIMARY KEY,
+//    service_name VARCHAR(255) NOT NULL,
+//    service_cost DECIMAL(10, 2) NOT NULL,
+//    service_category VARCHAR(255) NOT NULL,
+//    service_duration VARCHAR(255) NOT NULL,
+//    service_date DATE NOT NULL,
+//    service_next_date DATE NOT NULL,
+//    follow_up VARCHAR(255) NOT NULL
+//    pet_id INT NOT NULL,
+//    vet_id INT NOT NULL,
+//
+//)`
+//
+//const CreatePreventiveCareTable = `CREATE TABLE IF NOT EXISTS preventive_care(
+//    preventive_care_id INT AUTO_INCREMENT PRIMARY KEY,
+//    service_id INT NOT NULL,
+//    pet_id INT NOT NULL,
+//    vet_id INT NOT NULL,
+//    vaccination_type VARCHAR(255) NOT NULL,
+//    weight FLOAT NOT NULL,
+//
+//    FOREIGN KEY (service_id) REFERENCES service(service_id),
+//    FOREIGN KEY (pet_id) REFERENCES pet(pet_id),
+//    FOREIGN KEY (vet_id) REFERENCES veterinarian(vet_id)
+//)`
+//
+//const CreateGroomingTable = `CREATE TABLE IF NOT EXISTS grooming(
+//    grooming_id INT AUTO_INCREMENT PRIMARY KEY,
+//    service_id INT NOT NULL,
+//    pet_id INT NOT NULL,
+//    vet_id INT NOT NULL,
+//    grooming_type VARCHAR(255) NOT NULL,
+//
+//    FOREIGN KEY (service_id) REFERENCES service(service_id),
+//    FOREIGN KEY (pet_id) REFERENCES pet(pet_id),
+//    FOREIGN KEY (vet_id) REFERENCES veterinarian(vet_id)
+//)`
+//const CreateSurgeryTable = `CREATE TABLE IF NOT EXISTS surgery(
+//    surgery_id INT AUTO_INCREMENT PRIMARY KEY,
+//    service_id INT NOT NULL,
+//    pet_id INT NOT NULL,
+//    vet_id INT NOT NULL,
+//    anesthesia_used VARCHAR(255) NOT NULL,
+//    complications VARCHAR(255) NOT NULL,
+//    outcome VARCHAR(255) NOT NULL,
+//
+//    FOREIGN KEY (service_id) REFERENCES service(service_id),
+//    FOREIGN KEY (pet_id) REFERENCES pet(pet_id),
+//    FOREIGN KEY (vet_id) REFERENCES veterinarian(vet_id)
+//)`
+
+package queries
+
+const CreateOwnerTable = `CREATE TABLE IF NOT EXISTS owner (
+    owner_id INT AUTO_INCREMENT PRIMARY KEY,
+    owner_name VARCHAR(255) NOT NULL,
+    owner_address VARCHAR(255) NOT NULL,
+    owner_landline_number VARCHAR(255),
+    owner_mobile_number VARCHAR(255) NOT NULL,
+    owner_email_address VARCHAR(255) NOT NULL
+)`
+
+const CreatePetTable = `CREATE TABLE IF NOT EXISTS pet(
+    pet_id INT AUTO_INCREMENT PRIMARY KEY,
+    owner_id INT NOT NULL,
+    pet_name VARCHAR(255) NOT NULL,
+    pet_birth_date DATE NOT NULL,
+    pet_breed VARCHAR(255) NOT NULL,
+    pet_color VARCHAR(255) NOT NULL,
+    pet_type VARCHAR(255) NOT NULL,
+
+    FOREIGN KEY (owner_id) REFERENCES owner(owner_id)
+)`
+
+const CreateVeterinarianTable = `CREATE TABLE IF NOT EXISTS veterinarian(
+    vet_id INT AUTO_INCREMENT PRIMARY KEY,
+    vet_name VARCHAR(255) NOT NULL,
+    vet_email_address VARCHAR(50) NOT NULL,
+    vet_license_number VARCHAR(255) NOT NULL
+)`
+
+const CreateServiceTable = `CREATE TABLE IF NOT EXISTS service(
+    service_id INT AUTO_INCREMENT PRIMARY KEY,
+    service_name VARCHAR(255) NOT NULL,
+    service_cost DECIMAL(10, 2) NOT NULL,
+    service_category VARCHAR(255) NOT NULL,
+    service_duration VARCHAR(255) NOT NULL
+)`
+
+const CreateServiceInstanceTable = `CREATE TABLE IF NOT EXISTS service_instance(
+    service_instance_id INT AUTO_INCREMENT PRIMARY KEY,
+    service_date DATE NOT NULL,
+    service_next_date DATE,
+    follow_up VARCHAR(255) NOT NULL,
+    pet_id INT NOT NULL,
+    vet_id INT NOT NULL,
+    service_id INT NOT NULL,
+
+    FOREIGN KEY (service_id) REFERENCES service(service_id),
+    FOREIGN KEY (pet_id) REFERENCES pet(pet_id),
+    FOREIGN KEY (vet_id) REFERENCES veterinarian(vet_id)
+)`
+
+const CreatePreventiveCareTable = `CREATE TABLE IF NOT EXISTS preventive_care(
+    service_instance_id INT PRIMARY KEY,
+    vaccination_type VARCHAR(255),
+    weight FLOAT,
+
+    FOREIGN KEY (service_instance_id) REFERENCES service_instance(service_instance_id)
+)`
+
+const CreateSurgeryTable = `CREATE TABLE IF NOT EXISTS surgery(
+    service_instance_id INT PRIMARY KEY,
+    anesthesia_used VARCHAR(255),
+    complications VARCHAR(255),
+    outcome VARCHAR(255),
+
+    FOREIGN KEY (service_instance_id) REFERENCES service_instance(service_instance_id)
+)`
+
+const CreateGroomingTable = `CREATE TABLE IF NOT EXISTS grooming(
+    service_instance_id INT PRIMARY KEY,
+    grooming_type VARCHAR(255),
+
+    FOREIGN KEY (service_instance_id) REFERENCES service_instance(service_instance_id)
+)`
