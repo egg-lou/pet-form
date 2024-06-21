5c05477faad64cd851f00d5e99bc025456f66178
egglou <miguel.rafaellouie2003@gmail.com>
2024-06-21 19:40:51 +0800

    haha error page

---
+++ pet-client/src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { page } from "$app/stores";
+  const {status, error} = $page
+
+	console.log(status, error.message)
+</script>
+<div class="w-full h-full flex flex-col justify-center items-center gap-4">
+<h1 class="text-9xl">{status}</h1>
+<p class="text-4xl">{error.message}</p>
+<a href="/" class="btn variant-filled-error">Return to Home</a>
+</div>
--- pet-client/src/routes/+layout.svelte
+++ pet-client/src/routes/+layout.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import '../app.postcss';
-	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
-
+	import { AppShell, AppBar, setInitialClassState, getModeUserPrefers } from '@skeletonlabs/skeleton';
 	// Highlight JS
 	import hljs from 'highlight.js/lib/core';
 	import 'highlight.js/styles/github-dark.css';
@@ -10,25 +9,32 @@
 	import css from 'highlight.js/lib/languages/css';
 	import javascript from 'highlight.js/lib/languages/javascript';
 	import typescript from 'highlight.js/lib/languages/typescript';
-
+  // Import ThemeSwitcher
+	import { LightSwitch } from '@skeletonlabs/skeleton';
 	hljs.registerLanguage('xml', xml); // for HTML
 	hljs.registerLanguage('css', css);
 	hljs.registerLanguage('javascript', javascript);
 	hljs.registerLanguage('typescript', typescript);
 	storeHighlightJs.set(hljs);
-
+	import darkModeIcon from '../lib/assets/icons/darkmode-icon.png'
+	import lightModeIcon from '../lib/assets/icons/lightmode-icon.png'
+  let logo;
 	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 </script>
-
+<svelte:head>{@html '<script>(' + setInitialClassState.toString() + ')();</script>'}</svelte:head>
 <!-- App Shell -->
 <AppShell>
 	<svelte:fragment slot="header">
 		<!-- App Bar -->
-		<AppBar>
+		<AppBar class="px-10">
 			<svelte:fragment slot="lead">
+				<img
+					src={logo}
+					alt="Paws and Claws Logo"
+					class="w-10 h-10" />
 				<strong class="text-xl uppercase">Paws and Claws</strong>
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
@@ -38,6 +44,7 @@
 				>
 					Home
 				</a>
+				<LightSwitch />
 			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
