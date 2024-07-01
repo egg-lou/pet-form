f82b0bfd3099d9c87129fe7985477b1544cefd04
egglou <miguel.rafaellouie2003@gmail.com>
2024-07-01 23:13:35 +0800

    make footer have font-semibold

--- pet-app/components/ui/footer/Footer.vue
+++ pet-app/components/ui/footer/Footer.vue
@@ -3,7 +3,7 @@
 <template>
     <footer
         class="flex h-12 w-full items-center justify-center border-t-2 border-slate-400 bg-transparent px-6 backdrop-blur dark:border-slate-700 md:px-16">
-        <p class="text-center text-sm">© 2024 Paws and Claws</p>
+        <p class="text-center text-sm font-semibold">© 2024 Paws and Claws</p>
     </footer>
 </template>
 
