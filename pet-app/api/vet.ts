a09d6812f188eaacfdddc04a206c012a269d8d19
egglou <miguel.rafaellouie2003@gmail.com>
2024-07-03 11:13:56 +0800

    hehe

---
+++ pet-app/api/vet.ts
@@ -0,0 +1,19 @@
+import { AxiosService } from '~/composables/AxiosService'
+
+export class VetService extends AxiosService {
+  url = '/api/vet';
+  async getVets() {
+    try {
+      const response = await this.request(
+        'GET',
+        `${this.url}/get_vets`
+      );
+
+      const { data, status, statusText } = response;
+      return { data, status, statusText };
+    } catch (error ) {
+      console.error(error);
+      throw await Promise.reject(error);
+    }
+  }
+}
--- pet-app/pages/veterinarians/index.vue
+++ pet-app/pages/veterinarians/index.vue
@@ -1,4 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { VetService } from '~/api/vet';
+
+const vetService = new VetService();
+
+const fetchVets = async () => {
+    const response = await vetService.getVets();
+}
+
+onMounted(async () => {
+    await fetchVets();
+});
+
+</script>
 
 <template>
     <div class="main">hello</div>
